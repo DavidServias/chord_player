f05100881f7dd8acd23ddf39fa4ff35994909d9f
David <davidservias@gmail.com>
2019-11-11 21:22:02 -0800

    update style, add chord progression capability

--- recitative.js
+++ recitative.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
 
   const keyIds = ["c", "db", "d", "eb", "e", "f", "gb", "g", "ab", "a", "bb", "b" ];
-  const chord_qualities = ["major", "minor", "dom7", "dim"];
+  const chord_qualities = ["maj", "min", "7", "o"];
 
+  //CREATE HTML ELEMENTS
   //create single key
   const createKey = function(id){
     let container;
@@ -16,55 +17,22 @@ $(document).ready(function(){
     };
     let key = document.createElement("button");
     key.setAttribute("id", id);
-    key.setAttribute("class", "key");
+    key.setAttribute("class", "key col-sm");
     key.innerHTML = text;
     container.append(key);
   };
 
-  keyIds.forEach(function(id){
+  keyIds.forEach(function(id, index){
     createKey(id);
   });
 
+  //CREATE AUDIO ELEMENTS
   //create a single audio element
-  const createAudioElement = function myFunction(keyId, type) {
+  const createAudioElement = function myFunction(keyId, quality) {
     let el = document.createElement("audio");
-    let folder;
-    let id;
-    let src;
-    switch(type){
-      case "major":
-        id = keyId[0].toUpperCase();
-        //for flat keys
-        if (keyId.length > 1) {
-          id += "b";
-        };
-        id += "M";
-        folder = "audio/major_triads/";
-        src = folder + id + ".wav";
-        break;
-      case "minor":
-        id = keyId + "m";
-        folder = "audio/minor_triads/";
-        src = folder + id + ".wav";
-        break;
-      case "dom7":
-        id = keyId[0].toUpperCase();
-        //for flat keys
-        if (keyId.length > 1) {
-          id += "b";
-        };
-        id += "dom7";
-        folder = "audio/dom7_chords/";
-        src = folder + id + ".wav";
-        break;
-      case "dim":
-        id = keyId + "o";
-        folder = "audio/dim_triads/";
-        src = folder + id + ".wav";
-        break;
-    };
-    el.setAttribute("id", id + "-audio");
-    el.setAttribute("src", src);
+    let fileName = "audio/" + keyId + quality + ".wav";
+    el.setAttribute("id", keyId + quality + "-audio");
+    el.setAttribute("src", fileName);
     el.setAttribute("type", "audio/mpeg");
     document.body.appendChild(el);
   }
@@ -76,158 +44,215 @@ $(document).ready(function(){
     })
   });
 
-  function Chord_object(root, quality, abbreviation){
+  let Chord_object = function (root, quality){
     this.root = root;
     this.quality = quality;
-    this.abbreviation = abbreviation;
+    this.set_root = function(newRoot){
+      this.root = newRoot;
+      $(".flats .key").css("background-color", "black");
+      $(".naturals .key").css("background-color", "white");
+      $("#" + newRoot).css("background-color", "green");
+    };
+    this.set_quality = function(newQuality){
+      this.quality = newQuality;
+    };
     this.play_chord = function(){
-      let audio = $("audio#" + abbreviation + "-audio")[0];
+      let audio_string = "audio#" + this.root + this.quality + "-audio";
+      let audio = $(audio_string)[0];
       audio.currentTime = 0;
       audio.play();
     };
     this.pause_chord = function(){
-      let audio = $("audio#" + abbreviation + "-audio")[0];
+      let audio_string = "audio#" + this.root + this.quality + "-audio";
+      let audio = $(audio_string)[0];
       audio.pause();
     }
   };
 
   let chord_progression = {
-    current_chord:new Chord_object("C", "major", "CM"),
-    update_current_chord: function(Chord_object){
-      this.current_chord = Chord_object;
+    active_chord: new Chord_object("c", "maj", "CM"),
+    play_active: function(){
+      this.active_chord.play_chord();
+      this.last_chord = this.active_chord;
     },
-    chords:[],
-    add_chord: function(root, quality, abbreviation){
-      this.chords.push(new Chord_object(root, quality, abbreviation));
+    last_chord: new Chord_object("c", "maj", "CM"),
+    chords:[["c","maj"]],
+    set_active_quality: function(quality){
+      $(".quality").removeClass("active_quality");
+      $("#" + quality).addClass("active_quality");
+      this.active_chord.set_quality(quality);
     },
-    remove_last: ()=> {this.chords.pop();},
+    set_active_root: function(root){
+      this.active_chord.set_root(root);
+    },
+    add_chord: function(){
+      this.chords.push([this.active_chord.root, this.active_chord.quality]);
+      this.display_progression();
+    },
+    remove_last: function(){
+      if (this.chords.length === 0){
+      } else if(this.current_index === this.chords.length-1){
+          this.current_index -= 1;
+      }
+      this.chords.pop();
+      this.display_progression();
+    },
     current_index: 0,
     play_current:function() {
-      if (this.current_index !== 0) {
-        let previous = this.chords[this.current_index-1];
-        previous.pause_chord();
+      if (this.current_index === -1){
+        this.current_index = 0;
       };
-      let chord = this.chords[this.current_index];
-      chord.play_chord();
-      this.current_index += 1;
-      if (this.current_index >= this.chords.length){
-          this.current_index = 0;
-      };
+      let root = this.chords[this.current_index][0];
+      let quality = this.chords[this.current_index][1];
+      this.active_chord.set_root(root);
+      this.active_chord.set_quality(quality);
+      this.play_active();
+      this.last_chord = this.active_chord;
       this.display_progression();
-      console.log(chord.abbreviation);
+    },
+    play_next_in_progression: function() {
+      //advance the index if not last, and if chords > 1
+      if (this.chords.length > 1 && this.current_index !== this.chords.length-1) {
+        this.current_index += 1;
+      };
+      this.play_current();
+      this.display_progression();
+    },
+    play_prev: function() {
+      if (this.chords.length > 1 && this.current_index !== 0){//indicates first time
+        this.current_index -= 1;
+      };
+      this.play_current();
+      this.display_progression();
     },
     display_progression:function (){
-      let ci = this.current_index;
+      let active_index = this.current_index;
       $(".chord").remove();
+      let that = this;
       this.chords.forEach(function(chord, index){
-        let element = document.createElement("paragraph");
-        element.innerHTML = chord.abbreviation + "<br>";
-        element.setAttribute("class", "chord");
-        if (index === ci){
-          element.setAttribute("id", "current_chord");
-        }
+        let element;
+        let suffix;
+        let root;
+        let id = "index-" + index;
+        if (chord[0].length > 1) {
+          root = chord[0][0].toUpperCase() + chord[0][1];
+        } else {
+          root = chord[0].toUpperCase();
+        };
+        switch(chord[1]) {
+          case "maj":
+            suffix = "";
+            break;
+          case "min":
+            suffix ="m";
+            break;
+          default:
+            suffix = chord[1];
+            break;
+        };
+        element = $("<span></span>").text(root + suffix);
         $("#progression").append(element);
+        element.addClass("chord badge pill-badge badge-primary");
+        if (index === active_index){
+          element.attr("id","current_chord");
+        };
+        element.on("click", function(){
+          that.last_chord.pause_chord();
+          that.current_index = index;
+          that.display_progression();
+          that.play_current();
+        });
       });
     }
+
   };
 
-  //populates chord progression with "behold a virgin shall conceive"
-  chord_progression.add_chord("D", "major", "DM");
-  chord_progression.add_chord("A", "dom7", "Adom7");
-  chord_progression.add_chord("D", "major", "DM");
-  chord_progression.add_chord("Ab", "dim", "abo");
-  chord_progression.add_chord("A", "dim", "AM");
-  chord_progression.add_chord("D", "major", "DM");
-  chord_progression.add_chord("E", "major", "EM");
-  chord_progression.add_chord("A", "dim", "AM");
   chord_progression.display_progression();
-  //not sure if these are considered global variables, or if this bad practice.
-  var chord = $('audio#CM-audio')[0];
-  let quality = "major";
-
-  //add event listeners for major and minor buttons
-  //toggle background color to show active button
-  $("#major").on("click", function(){
-    $("#major").css("background-color", "blue");
-    $(".quality").css("background-color", "skyBlue");
-    quality = "major";
-  });
-
-  const selectQuality = function(chordType){
-    $(".quality").css("background-color", "skyBlue");
-    $("#"+chordType).css("background-color", "blue");
-    quality = chordType;
-  };
 
   //add click events to the quality buttons
   chord_qualities.forEach(function(quality){
-      $("#" + quality).on("click", () => selectQuality(quality));
+      $("#" + quality).on("click", function() {
+        chord_progression.set_active_quality(quality);
+      });
   });
 
   //pressing keys 1-4 also will select the quality
   $("body").on( "keydown", function(event) {
+    chord_progression.last_chord.pause_chord();
     let e = event.which;
     switch (e){
       case 49:
-        selectQuality("major");
+        chord_progression.set_active_quality("maj");
         break;
       case 50:
-        selectQuality("minor");
+        chord_progression.set_active_quality("min");
         break;
       case 51:
-        selectQuality("dom7");
+        chord_progression.set_active_quality("7");
         break;
       case 52:
-        selectQuality("dim");
+        chord_progression.set_active_quality("o");
         break;
-      case 32:
+      case 39:
+        chord_progression.play_next_in_progression();
+        break;
+      case 37:
+        chord_progression.play_prev();
+        break;
+      case 40:
         chord_progression.play_current();
+        break;
+      case 187://plus/=
+        chord_progression.add_chord();
+        break;
+      case 8://backspace
+      case 46://delete
+        chord_progression.remove_last();
         break;
     };
   });
 
-  //add event listeners to each button
-  var addAudioToButton = function(keyId) {
-    $("#" + keyId).click(function(){
-      chord.pause();
-      let major_chord;
-      let minor_chord;
-      let dom7_chord;
-      let root;
-      if (keyId.length > 1){
-        root = keyId[0].toUpperCase() + "b";
-        major_chord = root + "M-audio";
-        dom7_chord = root + "dom7-audio";
-      }
-      else {
-        root = keyId.toUpperCase();
-        major_chord = root + "M-audio";
-        dom7_chord = root.toUpperCase() + "dom7-audio";
-      };
-      minor_chord = keyId + "m-audio";
-      dim_chord = keyId + "o-audio";
-      switch(quality) {
-        case "major":
-          chord = $('audio#' + major_chord)[0];
-          break;
-        case "minor":
-          chord = $('audio#' + minor_chord)[0];
-          break;
-        case "dom7":
-          chord = $('audio#' + dom7_chord)[0];
-          break;
-        case "dim":
-          chord = $('audio#' + dim_chord)[0];
-          break;
-      };
-      chord.currentTime = 0;
-      chord.play();
+  function addEventListener(keyId){
+    $("#" + keyId).on("click", function(){
+      chord_progression.set_active_root(keyId);
+      $(".flats .key").css("background-color", "black");
+      $(".naturals .key").css("background-color", "white");
+      $("#" + keyId).css("background-color", "green");
     });
   };
 
-  //make add audio to each button.
-  keyIds.forEach(function(id) {
-    addAudioToButton(id);
+//make add audio to each button.
+  keyIds.forEach(function(id){
+    addEventListener(id);
   });
+
+  $("#play_chord").on("click", function(){
+    chord_progression.last_chord.pause_chord();
+    chord_progression.play_active();
+  });
+
+  $("#add_chord").on("click", function(){
+    chord_progression.add_chord();
+  });
+
+  $("#remove_last").on("click", function(){
+    chord_progression.remove_last();
+  });
+
+  $("#right_arrow").on("click", function(){
+    chord_progression.last_chord.pause_chord();
+    chord_progression.play_next_in_progression();
+  });
+
+  $("#left_arrow").on("click", function(){
+    chord_progression.last_chord.pause_chord();
+    chord_progression.play_prev();
+  });
+
+  $("#down_arrow").on("click", function(){
+    chord_progression.last_chord.pause_chord();
+    chord_progression.play_current();
+  });
+
 
 });
